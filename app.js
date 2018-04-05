090df3779c86d17d3c48a22d8ee8e9b01ccd0942
armin <arimin@qq.com>
2018-04-06 01:44:09 +0800

    support for https

--- app.js
+++ app.js
@@ -2,19 +2,33 @@
 console.log('loading...')
 
 var http = require('http');
+var https = require('https');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-var targetUrl, params, pageNum, startNum, urls = [];
+var targetUrl, params, pageNum, startNum, urls = [], HOST, UA, targetPath;
 
-targetUrl = 'http://www.haha.mx/topic/1/new/';
+// targetUrl = 'http://www.haha.mx/topic/1/new/';
+HOST = 'www.haha.mx',
+UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 params = '1',
-startNum = 55,
+startNum = 1,
+targetPath = '/topic/1/new/'
 pageNum = 20;
 
 function getHtml(url, params) {
     var pageData = '';
-    http.get(url + params, function(res) {
+
+    const options = {
+        host: HOST,
+        path: url + params,
+        'accept': '*/*',
+        headers: {
+            'User-Agent': UA
+        }
+    }
+
+    const req = https.get(options, function(res) {
         res.setEncoding('utf8');
 
         res.on('data', function(data) {
@@ -30,11 +44,11 @@ function getHtml(url, params) {
             var $ = cheerio.load(pageData);
 
              var html = $(".joke-list-item .joke-main-content a img");
-
              for (let i = html.length - 1; i >= 0; i--) {
+                 
                  var src = html[i].attribs.src;
 
-                 if (src.indexOf('http://image.haha.mx') > -1) {
+                 if (src.indexOf('image.haha.mx') > -1) {
                     urls.push(src);
                  }
              }
@@ -50,12 +64,18 @@ function getHtml(url, params) {
              }
         });
     });
+
+    req.on('error', console.log)
+    return req
 }
 
 function downloadImg(imgurl) {
-    var urlInfo = imgurl.replace("http://image.haha.mx/", "").split("/");
+    var urlInfo = imgurl.replace("//image.haha.mx/", "").split("/");
+    let targetImgUrl = 'http:' + imgurl
+    
+    targetImgUrl = targetImgUrl.replace("/small/", "/big/")
 
-    http.get(imgurl.replace("/small/", "/big/"), function(res) {
+    http.get(targetImgUrl, function(res) {
         var images = '';
 
         res.setEncoding('binary');
@@ -68,7 +88,7 @@ function downloadImg(imgurl) {
             var savePath = './download/2/' +  urlInfo[0]  + urlInfo[1] + urlInfo[2] + "_" + urlInfo[4];
 
             fs.writeFile(savePath, images, 'binary', function(err) {
-                err && console.log(err);
+                err && console.log('下载错误', err);
                 console.log(urlInfo[0]  + urlInfo[1] + urlInfo[2] + "_" + urlInfo[4]);
 
                 if (urls.length > 0) {
@@ -84,6 +104,6 @@ function downloadImg(imgurl) {
 init();
 function init() {
     for (var i = startNum; i <= (startNum + pageNum); i++) {
-        getHtml(targetUrl, i);
+        getHtml(targetPath, i);
     }
 }
